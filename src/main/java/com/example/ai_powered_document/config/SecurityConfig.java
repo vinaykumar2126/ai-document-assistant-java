1b10b778627fb2397843de04c696bf9ba5c0f4a5
vgodavarti <gvk.kumar100@gmail.com>
2025-11-30 17:21:06 -0500

    first-commit

---
+++ src/main/java/com/example/ai_powered_document/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.ai_powered_document.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterchain(HttpSecurity http)throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+    
+}
---
+++ src/test/java/com/example/ai_powered_document/AiPoweredDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_powered_document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPoweredDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
